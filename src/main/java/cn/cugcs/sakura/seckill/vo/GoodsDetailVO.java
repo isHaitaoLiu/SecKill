cd130eac65f040e7f5370bf37ad4f1df5b1fae2f
isHaitaoLiu <lht1970847795@gmail.com>
2021-10-28 15:36:53 +0800

    使用redis进行页面缓存

---
+++ src/main/java/cn/cugcs/sakura/seckill/vo/GoodsDetailVO.java
@@ -0,0 +1,13 @@
+package cn.cugcs.sakura.seckill.vo;
+/**
+* @program: seckill
+*
+* @description: 商品详情页面的返回对象
+*
+* @author: Sakura
+*
+* @create: 2021-10-28 15:35
+**/
+
+public class GoodsDetailVO {
+}
