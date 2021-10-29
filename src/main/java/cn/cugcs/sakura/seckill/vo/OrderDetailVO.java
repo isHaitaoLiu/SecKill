cc22c136c52afb18ed88d1dad6c80b8494defdfb
isHaitaoLiu <lht1970847795@gmail.com>
2021-10-29 17:19:20 +0800

    页面静态化及bug修复

---
+++ src/main/java/cn/cugcs/sakura/seckill/vo/OrderDetailVO.java
@@ -0,0 +1,13 @@
+package cn.cugcs.sakura.seckill.vo;
+/**
+* @program: seckill
+*
+* @description: 订单详情返回对象
+*
+* @author: Sakura
+*
+* @create: 2021-10-29 16:15
+**/
+
+public class OrderDetailVO {
+}
